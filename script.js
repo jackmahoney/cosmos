4d09804b4eb8a1693e757184b561716653486b27
Jack Mahoney <jackmahoney212@gmail.com>
2013-07-25 00:54:51 +1000

    first

---
+++ script.js
@@ -0,0 +1,94 @@
+(function($){
+
+    $(document).ready(load);
+
+    function load(){
+        var magnifier,
+            $window = $(window),
+            $body = $('body'),
+            dimens,
+            subject,
+            styleElement,
+            currentMode = 0,
+            modes = [],
+            css = 'body{cursor:none;}';
+
+
+        var Mode = function(name){
+            this.name = name;
+        };
+        Mode.prototype.setActive = function(){
+            subject.attr('class','');
+            subject.addClass(this.name);
+        };
+
+        function createModes(){
+            modes = [
+                new Mode('normal'),
+                new Mode('shift-red'),
+                new Mode('invert'),
+                new Mode('grey')
+            ];
+        }
+
+        //init
+        (function(){
+            wrapContent();
+            insertHTML();
+            injectStyles();
+            createModes();
+            attachMouse();
+            attachKeys();
+        })();
+
+        function attachKeys(){
+            $window.bind('keydown keypress',function(e){
+                if(e.which == 40 || e.which == 38){
+                    setMode(e.which == 38 ? -1 : 1);
+                    e.preventDefault();
+                    e.stopPropagation();
+                }
+            });
+        }
+
+        function wrapContent(){
+            $body.html('<div id="magnifier-subject">'+$body.html()+'</div>');
+            subject = $('#magnifier-subject');
+        }
+
+        function insertHTML(){
+            magnifier = $($('#magnifier-template').html()).appendTo($body);
+            dimens = {w:magnifier.width(),h:magnifier.height()};
+        }
+
+        function injectStyles(){
+            styleElement = $('<style type="text/css">'+css+'</style>').appendTo($body);
+        }
+
+        function attachMouse(){
+            $window.mousemove(function(e){
+                magnifier.css({
+                    top: e.pageY - dimens.h / 2,
+                    left: e.pageX - dimens.w / 2
+                });
+            });
+        }
+
+        function setMode(d){
+            var i;
+            if(arguments.length == 0 || d > 0){
+                i = currentMode + 1 < modes.length ? currentMode + 1 : 0;
+            }
+            else{
+                i = currentMode > 0 ? currentMode - 1 : modes.length -1;
+            }
+            $('.slides').toggleClass('rotate');
+            $('.slides').one('webkitAnimationEnd',function(){
+                modes[i].setActive();
+            })
+            currentMode = i;
+        }
+
+    }
+
+})(jQuery);
